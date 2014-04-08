b728b155154875f902833d5a3b578b3165df1f0a
Robin Deits <robin.deits@gmail.com>
2014-04-08 17:38:02 -0400

    some extra data for running terrain experiments

--- +test/test_terrain.m
+++ +test/test_terrain.m
@@ -1,4 +1,4 @@
-function test_terrain()
+function selected_points = test_terrain()
 
 import iris.terrain_grid.*;
 import iris.inflate_region;
@@ -7,7 +7,9 @@ import iris.cspace.*;
 import iris.drawing.*;
 lcmgl = drake.util.BotLCMGLClient(lcm.lcm.LCM.getSingleton(), 'segmentation');
 % load('example_feas_map.mat','Q');
-load('example_heights.mat','heights','px2world');
+load('data/example_heights.mat','heights','px2world');
+load('data/example_state.mat', 'x0');
+
 px2world(1,end) = px2world(1,end) - sum(px2world(1,1:3)); % stupid matlab 1-indexing...
 px2world(2,end) = px2world(2,end) - sum(px2world(2,1:3));
 resample = 2;
@@ -24,6 +26,7 @@ Q = Q | imfilter(heights, [-1, 1]) - (0.06/mag) > 0;
 Q = Q | imfilter(heights, [1; -1]) - (0.06/mag) > 0;
 Q = Q | imfilter(heights, [-1; 1]) - (0.06/mag) > 0;
 Q(isnan(heights)) = 1;
+
 % grid = Q(85:125,25:85);
 grid = ~Q;
 lb = [180;70;-pi];
@@ -36,19 +39,23 @@ A_bounds = [-1,0,0;
             0,0,1];
 b_bounds = [-lb;ub];
 
-bot = world2px_2x3 * 0.75*[-0.0820,-0.0820, 0.1780, 0.1780;
-                       0.0624,-0.0624, 0.0624,-0.0624;
-                       0,0,0,0];
+bot = world2px_2x3 * 0.9*bsxfun(@minus, [-0.0820,-0.0820, 0.1780, 0.1780;
+                                         0.0624,-0.0624, 0.0624,-0.0624;
+                                         0,0,0,0], [0.0480; 0; 0;]);
 % obstacles = segment_grid(~grid(lb(1):ub(1),lb(2):ub(2)));
 % for j = 1:length(obstacles)
 %   obstacles{j} = bsxfun(@plus, obstacles{j}, lb(1:2)-1);
 % end
 % obstacles = cspace3(obstacles, bot, 4);
+selected_points = [];
 
 while true
   figure(6)
   imshow(grid, 'InitialMagnification', 'fit')
   [c,r] = ginput(1);
+  if ~isempty(c)
+    selected_points(:,end+1) = [r;c];
+  end
   if isempty(c)
     break
   end
