7a7565258b4cf0951975ff9fbceb5eafe313292b
Robin Deits <robin.deits@gmail.com>
2014-04-25 18:18:05 -0400

    add automatic fallback to CVX
    (because mosek fusion is flaky with megaclear)

--- maximize_ellipse_in_polyhedron.m
+++ maximize_ellipse_in_polyhedron.m
@@ -4,21 +4,23 @@ import mosek_lownerjohn.lownerjohn_inner;
 dim = size(A,2);
 
 
-[C,d] = lownerjohn_inner(A,b);
-  
-%   cvx_begin sdp quiet
-%     cvx_solver Mosek
-%     variable C(dim,dim) semidefinite
-%     variable d(dim)
-%   %   maximize(log_det(C));
-%     maximize(det_rootn(C))
-%   %     maximize(trace(C))
-%     subject to
-%       for i = 1:length(b)
-%         [(b(i) - A(i,:) * d) * eye(dim), C * (A(i,:)');
-%          (C * (A(i,:)'))', (b(i) - A(i,:) * d)] >= 0;
-%       end
-%   cvx_end
+try
+  [C,d] = lownerjohn_inner(A,b);
+catch exception
+  disp(getReport(exception));
+  fprintf(1, 'Warning: Mosek Fusion call failed. Falling back to CVX');
+  cvx_begin sdp quiet
+    cvx_solver Mosek
+    variable C(dim,dim) semidefinite
+    variable d(dim)
+    maximize(det_rootn(C))
+    subject to
+      for i = 1:length(b)
+        [(b(i) - A(i,:) * d) * eye(dim), C * (A(i,:)');
+         (C * (A(i,:)'))', (b(i) - A(i,:) * d)] >= 0;
+      end
+  cvx_end
+end
 
 volume = det(C);
 
