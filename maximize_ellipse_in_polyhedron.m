fc4e2ed3734aad336ee2a4c3ca3eccc49be3b03c
Robin Deits <robin.deits@gmail.com>
2014-04-29 19:53:15 -0400

    use updated mosek fusion code

--- maximize_ellipse_in_polyhedron.m
+++ maximize_ellipse_in_polyhedron.m
@@ -1,12 +1,12 @@
 function [C, d, volume] = maximize_ellipse_in_polyhedron(A,b,C,d)
-import mosek_lownerjohn.lownerjohn_inner;
+import mosek_lownerjohn.lownerjohn_ellipsoid;
 
 dim = size(A,2);
 
 
-try
-  [C,d] = lownerjohn_inner(A,b);
-catch exception
+% try
+  [C,d] = lownerjohn_ellipsoid.lownerjohn_inner(A,b);
+% catch exception
   disp(getReport(exception));
   fprintf(1, 'Warning: Mosek Fusion call failed. Falling back to CVX');
   cvx_begin sdp quiet
@@ -20,7 +20,7 @@ catch exception
          (C * (A(i,:)'))', (b(i) - A(i,:) * d)] >= 0;
       end
   cvx_end
-end
+% end
 
 volume = det(C);
 
