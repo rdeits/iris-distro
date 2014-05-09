788b65edd4c8d6e1dfbb4c1a1fe3c765e6534551
Robin Deits <robin.deits@gmail.com>
2014-05-09 19:06:40 -0400

    support iteration limit

--- python/irispy/iris.py
+++ python/irispy/iris.py
@@ -5,23 +5,24 @@ import mosek.fusion
 from irispy.hyperplanes import compute_obstacle_planes
 from irispy.mosek_ellipsoid.lownerjohn_ellipsoid import lownerjohn_inner
 
-def inflate_region(obstacle_pts, A_bounds, b_bounds, start, require_containment=False):
+def inflate_region(obstacle_pts, A_bounds, b_bounds, start, require_containment=False, iter_limit=np.inf):
     A_bounds = np.array(A_bounds)
     b_bounds = np.array(b_bounds)
     d = np.array(start)
     dim = A_bounds.shape[1]
     C = 0.01 * np.eye(dim)
-    best_vol = -1
+    best_vol = 1e-100
     results = {'p_history': [], 'e_history': []}
-    iter = 1
+    iters = 1
 
     while True:
         A, b, infeas_start = compute_obstacle_planes(obstacle_pts, C, d)
+        print "number of hyperplanes:", len(b)
         A = np.vstack((A, A_bounds))
         b = np.hstack((b, b_bounds))
 
         if require_containment:
-            if np.all(A.dot(start) <= b) or iter == 1 or infeas_start:
+            if np.all(A.dot(start) <= b) or iters == 1 or infeas_start:
                 results['p_history'].append({'A': A, 'b': b})
             else:
                 A = results['p_history'][-1]['A']
@@ -30,6 +31,12 @@ def inflate_region(obstacle_pts, A_bounds, b_bounds, start, require_containment=
                 break
         else:
             results['p_history'].append({'A': A, 'b': b})
+
+        iters += 1
+
+        if iters > iter_limit:
+            print "iter limit reached"
+            break
 
         try:
             C, d = lownerjohn_inner(A, b)
@@ -46,7 +53,6 @@ def inflate_region(obstacle_pts, A_bounds, b_bounds, start, require_containment=
         if abs(vol - best_vol) / best_vol < 2e-2:
             break
         best_vol = vol
-        iter += 1
     return A, b, C, d, results
 
 
