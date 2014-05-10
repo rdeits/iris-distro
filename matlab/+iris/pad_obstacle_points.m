7617577dabb4a6478742964c5947e9c11007cc9f
Robin Deits <robin.deits@gmail.com>
2014-05-10 19:05:49 -0400

    only pad if necessary

--- matlab/+iris/pad_obstacle_points.m
+++ matlab/+iris/pad_obstacle_points.m
@@ -1,17 +1,24 @@
 function padded_obstacles = pad_obstacle_points(obstacles)
 
 padded_obstacles = obstacles;
+needs_pad = false;
 max_len = 0;
 for j = 1:length(obstacles)
   l = size(obstacles{j},2);
   if l > max_len
     max_len = l;
+  elseif l < max_len
+    needs_pad = true;
   end
 end
 
-for j = 1:length(obstacles)
-  if size(obstacles{j}, 2) < max_len
-    obs = obstacles{j};
-    padded_obstacles{j} = [obs, repmat(obs(:,end), 1, max_len - size(obs,2))];
+if needs_pad
+  for j = 1:length(obstacles)
+    if size(obstacles{j}, 2) < max_len
+      obs = obstacles{j};
+      padded_obstacles{j} = [obs, repmat(obs(:,end), 1, max_len - size(obs,2))];
+    end
   end
+else
+  padded_obstacles = obstacles;
 end
