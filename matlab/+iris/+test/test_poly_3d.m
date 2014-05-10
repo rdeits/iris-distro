155845692cf52cf1712b57420032305bf8d41a46
Robin Deits <robin.deits@gmail.com>
2014-05-10 16:46:21 -0400

    cleanup

--- matlab/+iris/+test/test_poly_3d.m
+++ matlab/+iris/+test/test_poly_3d.m
@@ -29,8 +29,9 @@ b_bounds = [-lb;ub];
 start = 0.5 * (ub + lb);
 
 
-
+profile on
 [A,b,C,d,results] = iris.inflate_region(obstacles, A_bounds, b_bounds, start);
-animate_results(results, record);
+profile viewer
+% animate_results(results, record);
 
 end
