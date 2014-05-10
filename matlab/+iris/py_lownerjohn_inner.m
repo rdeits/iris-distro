2ab23e2b4e5ab9d4c348a8ee722382fe5c192a5b
Robin Deits <robin.deits@gmail.com>
2014-05-10 16:44:03 -0400

    use updated py package

--- matlab/+iris/py_lownerjohn_inner.m
+++ matlab/+iris/py_lownerjohn_inner.m
@@ -2,7 +2,7 @@ function [C, d] = py_lownerjohn_inner(A, b)
 
 fname = tempname();
 save(fname, 'A', 'b');
-system(['python -m irispy.mosek.matlab_wrapper ', fname]);
+system(['python -m irispy.mosek_ellipsoid.matlab_wrapper ', fname]);
 load(fname, 'C', 'd');
 d = reshape(d, [], 1);
 
