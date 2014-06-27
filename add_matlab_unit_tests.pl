7ba8492d8276a52986b2f1e7ca7b0edb6eacc236
Russ Tedrake <russt@mit.edu>
2014-06-27 06:20:37 -0400

    support test-specific timeouts for matlab unit tests

--- add_matlab_unit_tests.pl
+++ add_matlab_unit_tests.pl
@@ -17,7 +17,7 @@ open(my $in, 'pod-build/matlab_ctests');
 open(my $ctestfile, '>>', 'pod-build/CTestTestfile.cmake');
 
 while (<$in>) {
-  ($test,$testdir) = split(' ');
+  ($test,$testdir,$timeout) = split(' ');
   $testname = $testdir."/".$test;
   $testname =~ s/\Q$CMAKE_SOURCE_DIR\///;
 
@@ -25,5 +25,9 @@ while (<$in>) {
   $failcondition = "~strncmp(ex.identifier,'Drake:MissingDependency',23)";  # missing dependency => pass
 
   print $ctestfile "ADD_TEST($testname \"$CMAKE_SOURCE_DIR/cmake/matlab_clean.pl\" \"-nosplash\" \"-nodisplay\" \"-r\" \"addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; rng('shuffle'); rng_state=rng; global g_disable_botvis; g_disable_botvis=true; try, feval('$test'); catch ex, disp(getReport(ex,'extended')); disp(''); disp(sprintf('To reproduce this test use rng(%d,''%s'')',rng_state.Seed,rng_state.Type)); knownIssue('$testname',ex); force_close_system; exit($failcondition); end; force_close_system; exit(0)\")\n";
-  print $ctestfile "SET_TESTS_PROPERTIES($testname PROPERTIES  WORKING_DIRECTORY \"$testdir\")\n";
+  $props = "WORKING_DIRECTORY \"$testdir\" ";
+  if ($timeout) {
+      $props = $props . "TIMEOUT " . $timeout . " ";
+  } 
+  print $ctestfile "SET_TESTS_PROPERTIES($testname PROPERTIES " . $props .")\n";
 }
