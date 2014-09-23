c8c604997bb624033c4dee7f9fab54bc2c403f7a
Robin Deits <robin.deits@gmail.com>
2014-09-23 13:14:43 -0400

    handle 3d case

--- matlab/+iris/+cspace/minkowski_sum.m
+++ matlab/+iris/+cspace/minkowski_sum.m
@@ -1,6 +1,6 @@
 function p = minkowski_sum(a, b)
 
-p = zeros(2, size(a,2) * size(b,2));
+p = zeros(size(a, 1), size(a,2) * size(b,2));
 idx = 1;
 for j = 1:size(a,2)
   for k = 1:size(b,2)
@@ -9,6 +9,13 @@ for j = 1:size(a,2)
   end
 end
 
-k = convhull(p(1,:), p(2,:), 'simplify', true);
-assert(k(1) == k(end));
-p = p(:,k(1:end-1));
+if size(p, 1) == 2
+  k = convhull(p(1,:), p(2,:), 'simplify', true);
+  assert(k(1) == k(end));
+  p = p(:,k(1:end-1));
+elseif size(p, 1) == 3
+  k = convhull(p(1,:), p(2,:), p(3,:), 'simplify', true);
+  p = p(:,k);
+  p = unique(p', 'rows')';
+end
+
