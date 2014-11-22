010762c17b70d50a9c89aa40f68e4cace6c58fa5
Russ Tedrake <russt@mit.edu>
2014-11-22 17:50:03 -0500

    adds support for unit testing on windows. (btw - this took me *way* too long)

--- add_matlab_unit_tests.pl
+++ add_matlab_unit_tests.pl
@@ -6,12 +6,24 @@ if ($#ARGV>-1) {
   chdir($ARGV[0]);
 }
 
-$CMAKE_INSTALL_PREFIX = `cmake pod-build -L | grep CMAKE_INSTALL_PREFIX | cut -d "=" -f2 | tr -d '[:space:]'`;
-$CMAKE_SOURCE_DIR = `pwd | tr -d '[:space:]'`;
-$POD_NAME = `cmake pod-build -L | grep POD_NAME | cut -d "=" -f2 | tr -d '[:space:]'`;
+chomp($CMAKE_INSTALL_PREFIX = `cmake pod-build -L | grep CMAKE_INSTALL_PREFIX | cut -d "=" -f2`);
+chomp($CMAKE_SOURCE_DIR = `pwd`);
+chomp($POD_NAME = `cmake pod-build -L | grep POD_NAME | cut -d "=" -f2`);
 
-# write unit tests to .matlab_ctests
-system("matlab -nodisplay -r \"addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; options.gui = false; options.autorun = false; options.test_list_file = 'pod-build/matlab_ctests'; unitTest(options); exit;\"");
+if ($^O eq 'cygwin') {
+  chomp($CMAKE_INSTALL_PREFIX = `cygpath -w $CMAKE_INSTALL_PREFIX`);
+  chomp($CMAKE_SOURCE_DIR = `cygpath -w $CMAKE_SOURCE_DIR`);
+  $matlab_cmd = "matlab -wait -nodesktop -nosplash";
+  chomp($perl_cmd = `which perl`);
+  chomp($perl_cmd = `cygpath -w $perl_cmd`);
+  $perl_cmd = "\"$perl_cmd\"";
+} else {
+  $matlab_cmd = "matlab -nodisplay -nosplash";
+  $perl_cmd = "";
+}
+
+# write unit tests to pod-build/matlab_ctests
+system("$matlab_cmd -r \"addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; options.gui = false; options.autorun = false; options.test_list_file = 'pod-build/matlab_ctests'; unitTest(options); exit;\"");
 
 open(my $in, 'pod-build/matlab_ctests');
 open(my $ctestfile, '>>', 'pod-build/CTestTestfile.cmake');
@@ -24,7 +36,8 @@ while (<$in>) {
 #  $failcondition = "1";   # missing dependency => failure
   $failcondition = "~strncmp(ex.identifier,'Drake:MissingDependency',23)";  # missing dependency => pass
 
-  print $ctestfile "ADD_TEST($testname \"$CMAKE_SOURCE_DIR/cmake/matlab_clean.pl\" \"-nosplash\" \"-nodisplay\" \"-r\" \"rng('shuffle'); rng_state=rng; disp(sprintf('To reproduce this test use rng(%d,''%s'')',rng_state.Seed,rng_state.Type)); disp(' '); addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; global g_disable_visualizers; g_disable_visualizers=true; try, fevalPackageSafe('$test'); catch ex, disp(getReport(ex,'extended')); disp(' '); fprintf('<test_name>$testname</test_name> <error_id>%s</error_id> <error_message>%s</error_message>',ex.identifier,ex.message); disp(' '); force_close_system; exit($failcondition); end; force_close_system; exit(0)\")\n";
+  print $ctestfile "ADD_TEST($testname $perl_cmd \"$CMAKE_SOURCE_DIR/cmake/matlab_clean.pl\" \"-r\" \"rng('shuffle'); rng_state=rng; disp(sprintf('To reproduce this test use rng(%d,''%s'')',rng_state.Seed,rng_state.Type)); disp(' '); addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; global g_disable_visualizers; g_disable_visualizers=true; try, fevalPackageSafe('$test'); catch ex, disp(getReport(ex,'extended')); disp(' '); fprintf('<test_name>%s</test_name> <error_id>%s</error_id> <error_message>%s</error_message>','$testname',ex.identifier,ex.message); disp(' '); force_close_system; exit($failcondition); end; force_close_system; exit(0)\")\n";
+
   $props = "WORKING_DIRECTORY \"$testdir\" $props";
   print $ctestfile "SET_TESTS_PROPERTIES($testname PROPERTIES " . $props .")\n";
 }
--- matlab_clean.pl
+++ matlab_clean.pl
@@ -12,13 +12,36 @@
 #use Text::Unidecode;
 
 my $cmd = 'matlab';
+
 foreach my $a(@ARGV) {
   $cmd .= " \"$a\"";
 }
+
+my $osname = $^O;
+
+if ($osname eq "cygwin" || $osname eq "MSWin32") {
+  $tmpfile = "c:\\tmp\\" . time() . "_" . int(rand(100000));
+  $cmd .= " -wait -nosplash -nodesktop -logfile \"$tmpfile\"";    
+} else {
+  $cmd .= " -nosplash -nodisplay";
+}
 $cmd .= ' 2>&1 ';
+
+print($cmd);
 
 my $matlab_output = `$cmd`;
 my $retval = $? >> 8;
+
+if ($osname eq "cygwin" || $osname eq "MSWin32") {
+  # read entire file in (using a trick from http://www.perlmonks.org/?node_id=1952)  
+  local $/=undef;
+  open FILE, "$tmpfile" or die "Couldn't open file: $!";
+  $matlab_output = <FILE>;
+  close FILE;
+
+  # rm the temp file
+  unlink $tmpfile;
+} 
 
 $matlab_output =~ s/.*exclude an item from Time Machine.*\n//g;
 #$matlab_output = unidecode($matlab_output);  # didn't seem to help
