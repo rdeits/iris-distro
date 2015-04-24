2cb783eed7850b29d33356162ccf848e508e33b2
Russ Tedrake <russt@mit.edu>
2015-04-24 07:56:29 -0400

    resolves cmake warning spew on windows

--- add_matlab_unit_tests.pl
+++ add_matlab_unit_tests.pl
@@ -11,12 +11,12 @@ chomp($CMAKE_SOURCE_DIR = `pwd`);
 chomp($POD_NAME = `cmake pod-build -N -L | grep POD_NAME | cut -d "=" -f2`);
 
 if ($^O eq 'cygwin') {
-  chomp($CMAKE_INSTALL_PREFIX = `cygpath -w $CMAKE_INSTALL_PREFIX`);
-  chomp($CMAKE_SOURCE_DIR = `cygpath -w $CMAKE_SOURCE_DIR`);
+  chomp($CMAKE_INSTALL_PREFIX = `cygpath -m $CMAKE_INSTALL_PREFIX`);
+  chomp($CMAKE_SOURCE_DIR = `cygpath -m $CMAKE_SOURCE_DIR`);
   $matlab_cmd = "matlab -wait -nodesktop -nosplash";
-  $matlab_clean_cmd = "$CMAKE_SOURCE_DIR\cmake\matlab_clean.pl";
+  $matlab_clean_cmd = "$CMAKE_SOURCE_DIR/cmake/matlab_clean.pl";
   chomp($perl_cmd = `which perl`);
-  chomp($perl_cmd = `cygpath -w $perl_cmd`);
+  chomp($perl_cmd = `cygpath -m $perl_cmd`);
   $perl_cmd = "\"$perl_cmd\"";
 } else {
   $matlab_cmd = "matlab -nodisplay -nosplash";
@@ -32,14 +32,14 @@ open(my $ctestfile, '>>', 'pod-build/CTestTestfile.cmake');
 
 while (<$in>) {
   ($test,$testdir,$props) = split(' ',$_,3);
+  $testdir =~ s/\\/\//g;
   $testname = $testdir."/".$test;
   $testname =~ s/\Q$CMAKE_SOURCE_DIR\E[\/\\]//;
-  $testname =~ s/\\/\//g;
 
 #  $failcondition = "1";   # missing dependency => failure
   $failcondition = "~strncmp(ex.identifier,'Drake:MissingDependency',23)";  # missing dependency => pass
 
-  print $ctestfile "ADD_TEST($testname $perl_cmd \"$matlab_clean_command\" \"-r\" \"rng('shuffle'); rng_state=rng; disp(sprintf('To reproduce this test use rng(%d,''%s'')',rng_state.Seed,rng_state.Type)); disp(' '); addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; global g_disable_visualizers; g_disable_visualizers=true; try, fevalPackageSafe('$test'); catch ex, disp(getReport(ex,'extended')); disp(' '); fprintf('<test_name>%s</test_name> <error_id>%s</error_id> <error_message>%s</error_message>','$testname',ex.identifier,ex.message); disp(' '); force_close_system; exit($failcondition); end; force_close_system; exit(0)\")\n";
+  print $ctestfile "ADD_TEST($testname $perl_cmd \"$matlab_clean_cmd\" \"-r\" \"rng('shuffle'); rng_state=rng; disp(sprintf('To reproduce this test use rng(%d,''%s'')',rng_state.Seed,rng_state.Type)); disp(' '); addpath('$CMAKE_INSTALL_PREFIX/matlab'); addpath_$POD_NAME; global g_disable_visualizers; g_disable_visualizers=true; try, fevalPackageSafe('$test'); catch ex, disp(getReport(ex,'extended')); disp(' '); fprintf('<test_name>%s</test_name> <error_id>%s</error_id> <error_message>%s</error_message>','$testname',ex.identifier,ex.message); disp(' '); force_close_system; exit($failcondition); end; force_close_system; exit(0)\")\n";
 
   $props = "WORKING_DIRECTORY \"$testdir\" $props";
   print $ctestfile "SET_TESTS_PROPERTIES($testname PROPERTIES " . $props .")\n";
