8401d6f033ca4ef542c5b544d7e4140bf44bc310
Robin Deits <robin.deits@gmail.com>
2015-07-16 19:04:45 -0400

    fix platform specific issues

--- python/irispy/setup.py
+++ python/irispy/setup.py
@@ -2,6 +2,7 @@ from distutils.core import setup, Extension
 from Cython.Build import cythonize
 
 import numpy
+import sys
 
 setup(ext_modules = cythonize(Extension(
     "iriscore",
@@ -9,6 +10,6 @@ setup(ext_modules = cythonize(Extension(
     language="c++",
     include_dirs=[numpy.get_include(), "."],
     libraries=["iris", "iris_mosek", "mosek64", "iris_cvxgen_ldp_cpp"],
-    extra_compile_args=["-std=c++11", "-stdlib=libc++"],
+    extra_compile_args=["-std=c++11"] + (["-stdlib=libc++"] if "darwin" in sys.platform else []),
     extra_link_args=["-std=c++11"],
         )))
