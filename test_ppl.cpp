d3a402c964cb54c4b71a17613f9c54b019e4a78e
Robin Deits <robin.deits@gmail.com>
2015-07-16 17:08:20 -0400

    add a ppl test

---
+++ test_ppl.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include "ppl.hh"
+
+using namespace Parma_Polyhedra_Library;
+
+int main(int argc, char **argv) {
+  std::cout << "hello world" << std::endl;
+
+  Variable x(0);
+  Variable y(1);
+
+  return 0;
+}
