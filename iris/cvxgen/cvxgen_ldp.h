a197d6b8c6c0d077e88172824fee64a07c8803bf
Robin Deits <robin.deits@gmail.com>
2015-07-29 17:51:55 -0400

    remove iris so we can re-add without squashing

--- iris/cvxgen/cvxgen_ldp.h
+++
@@ -1,9 +0,0 @@
-#ifndef _CVXGEN_LDP_H
-#define _CVXGEN_LDP_H
-
-#define IRIS_CVXGEN_LDP_MAX_ROWS 3
-#define IRIS_CVXGEN_LDP_MAX_COLS 8
-
-void iris_cvxgen_closest_point_in_convex_hull(double *Y, double *v);
-
-#endif
--- iris/matlab/+iris/+inner_ellipsoid/py_mosek_ellipsoid.m
+++
@@ -1,9 +0,0 @@
-function [C, d] = py_mosek_ellipsoid(A, b)
-
-fname = tempname();
-save(fname, 'A', 'b');
-system(['python -m irispy.mosek_ellipsoid.matlab_wrapper ', fname]);
-load(fname, 'C', 'd');
-d = reshape(d, [], 1);
-
-end
