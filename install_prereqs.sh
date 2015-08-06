7489c61cc1706ba94db71c1a96d5edfe672f1ee4
Robin Deits <robin.deits@gmail.com>
2015-08-06 18:42:19 -0400

    add virtualenv

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -2,27 +2,13 @@
 
 case $1 in
   ("homebrew")
-    brew install cmake pkg-config gmp openblas && easy_install pip ;;
-  ("macports")
-    port install cmake gmp openblas && easy_install pip;;
+    brew install cmake pkg-config gmp openblas && easy_install pip && pip install virtualenv ;;
   ("ubuntu")
-    apt-get install cmake build-essential libgmp-dev python-pip liblapack-dev libblas-dev gfortran ;;
-  ("cygwin")
-    cygwin-setup -q -P make pkg-config libgmp-devel ;;
+    apt-get install cmake build-essential libgmp-dev python-pip liblapack-dev libblas-dev gfortran python-virtualenv ;;
   (*)
     echo "Usage: ./install_prereqs.sh package_manager"
     echo "where package_manager is one of the following: "
     echo "  homebrew"
-    echo "  macports"
     echo "  ubuntu"
-    echo "  cygwin"
     exit 1 ;;
 esac
-
-SUBDIRS="drake externals"
-for subdir in $SUBDIRS; do
-  if [ -f $subdir/install_prereqs.sh ]; then
-    echo "installing prereqs for $subdir"
-    ( cd $subdir; ./install_prereqs.sh $1 || true )
-  fi
-done
