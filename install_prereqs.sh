cee30c86b5bc029d403315bd6293740a11747a35
Robin Deits <robin.deits@gmail.com>
2015-08-07 00:03:20 -0400

    use apt-get for python again

--- install_prereqs.sh
+++ install_prereqs.sh
@@ -2,9 +2,9 @@
 
 case $1 in
   ("homebrew")
-    brew install cmake pkg-config gmp openblas && easy_install pip && pip install virtualenv ;;
+    brew install cmake pkg-config gmp openblas && easy_install pip && pip install -r python_requirements.txt ;;
   ("ubuntu")
-    apt-get install cmake build-essential libgmp-dev python-pip liblapack-dev libblas-dev gfortran python-virtualenv ;;
+    apt-get install cmake build-essential libgmp-dev python-pip liblapack-dev libblas-dev gfortran python-scipy python-numpy python-matplotlib python-nose ;;
   (*)
     echo "Usage: ./install_prereqs.sh package_manager"
     echo "where package_manager is one of the following: "
