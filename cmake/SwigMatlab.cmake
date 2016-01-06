25f3926f997a1231542fc004d2dffff9a935e5ea
Robin Deits <robin.deits@gmail.com>
2016-01-05 23:20:25 -0500

    Revert "don't mark swig as required because findSWIG on windows is borked"
    This reverts commit 4a1a63fb578e0f25c176e5866bfa99c6cbb2b4ed.

--- cmake/SwigMatlab.cmake
+++ cmake/SwigMatlab.cmake
@@ -45,7 +45,7 @@ function(add_swig_matlab_module target i_file)
 
 	# Load the swig macros
 	if (NOT SWIG_EXECUTABLE)
-		find_package(SWIG)
+		find_package(SWIG REQUIRED)
 		include(DrakeUseSWIG)
 	endif()
 
