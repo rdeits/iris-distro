79c6474cfcf83885f68b3aeb976531d1fd4b4d79
Robin Deits <robin.deits@gmail.com>
2017-01-26 15:10:33 -0500

    try harder

--- travis/linux.sh
+++ travis/linux.sh
@@ -1,12 +1,13 @@
-set -ex
+set -e
 
 mkdir -p ~/tools
-cd ~/tools
+pushd ~/tools
+
 wget https://cmake.org/files/v3.5/cmake-3.5.2-Linux-x86_64.tar.gz
 tar zxvf cmake-3.5.2-Linux-x86_64.tar.gz
 rm cmake-3.5.2-Linux-x86_64.tar.gz
 cd cmake-3.5.2-Linux-x86_64/bin
 export PATH=`pwd`:$PATH
-cd -
+popd
 
 sudo apt-get install -y pkg-config libgmp-dev
