f9f1fab3f46b253f9acf5ef9823728404a51f75b
Robin Deits <robin.deits@gmail.com>
2017-10-04 15:03:16 -0400

    wrap inner_ellipsoid from python

--- src/python/irispy/__init__.py
+++ src/python/irispy/__init__.py
@@ -1,3 +1,9 @@
 from __future__ import absolute_import
 
-from .irispy import inflate_region, Polyhedron, Ellipsoid, IRISRegion, IRISOptions, IRISProblem
+from .irispy import (inflate_region,
+                     inner_ellipsoid,
+                     Polyhedron,
+                     Ellipsoid,
+                     IRISRegion,
+                     IRISOptions,
+                     IRISProblem)
--- src/python/irispy/test/test_ellipsoid.py
+++ src/python/irispy/test/test_ellipsoid.py
@@ -13,3 +13,14 @@ class EllipsoidTest(TestCase):
         e = irispy.Ellipsoid.fromNSphere(np.array([5.0, 0.1,-1000]), 0.5)
         self.assertAlmostEqual(e.getVolume(), 4.0 / 3.0 * np.pi * 0.5 ** 3)
         self.assertAlmostEqual(e.getC()[1,1], 0.5)
+
+    def test_inner_ellipsoid(self):
+        # polyhedron from [-1.3, -1.4] to [1.1, 1.2]
+        # center is at [-0.1, -0.1]
+        A = np.vstack((np.eye(2),
+                       -np.eye(2)))
+        b = np.array([1.1, 1.2, 1.3, 1.4])
+        p = irispy.Polyhedron(A, b)
+        e = irispy.inner_ellipsoid(p)
+        self.assertTrue(np.allclose(e.getD(), [-0.1, -0.1]))
+        self.assertTrue(np.allclose(e.getC(), np.array([[1.2, 0], [0, 1.3]])))
